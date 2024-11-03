7a83b3f73e4cc63de8401659de8cb0b0e561afe6
vanHp209 <van.borysov@gmail.com>
2024-11-03 15:21:43 +0200

    Add files via upload

---
+++ Hw-5/Hw 5.py
@@ -0,0 +1,16 @@
+class Charapter:
+    def __init__(self, name, healht):
+        def __init__(self, name, health):
+            self.name = name
+            self.health = health
+        attack()
+
+class Enptemy(Character):
+    def __init__(self, name, health, damage):
+        super().__init__(damage)
+        self.damage = damage
+
+class Hero(Character):
+    def __init__(self, vearpon):
+        super().__init__(name, health)
+        self.weapon = weapon
